1661168b5f172acae3958e8a9ae49ea7ee6a73c0
Remi_Petitjean <133965907+rpetitjean@users.noreply.github.com>
2025-10-05 18:29:10 +0200

    Add files via upload

--- minimal-painter.js
+++ minimal-painter.js
@@ -753,12 +753,7 @@ AFRAME.registerComponent('size-picker',{
 AFRAME.registerComponent('color-picker',{
   schema:{
     colors:{ default:[
-      '#ffffff','#000000','#ff8000','#ffbf00',
-      '#ffff00','#bfff00','#80ff00','#40ff00',
-      '#00ff00','#00ff40','#00ff80','#00ffbf',
-      '#00ffff','#00bfff','#0080ff','#0040ff',
-      '#0000ff','#4000ff','#8000ff','#bf00ff',
-      '#ff00ff','#ff00bf','#ff0080','#ff0040'
+
     ]},
     bgRadius:  { default: 0.11 },
     bgColor:   { default: '#ffffff' },
