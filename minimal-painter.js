b4aab45c6b87be9cf369deb9a63f98e0f6321dac
Remi_Petitjean <133965907+rpetitjean@users.noreply.github.com>
2025-10-05 18:23:50 +0200

    Add files via upload

--- minimal-painter.js
+++ minimal-painter.js
@@ -31,12 +31,7 @@ AFRAME.registerComponent('spatial-marker', {
 
     // --- Color-picker passthrough (applied only once inside) ---
     colors:          { default: [
-      '#ffffff','#000000','#ff8000','#ffbf00',
-      '#ffff00','#bfff00','#80ff00','#40ff00',
-      '#00ff00','#00ff40','#00ff80','#00ffbf',
-      '#00ffff','#00bfff','#0080ff','#0040ff',
-      '#0000ff','#4000ff','#8000ff','#bf00ff',
-      '#ff00ff','#ff00bf','#ff0080','#ff0040'
+
     ]},
 
     // --- Locomotion / rig targeting ---
